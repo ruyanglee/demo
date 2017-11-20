4b359bbec5ace14004787f8a6f0a0b90073f6224
ruyanglee <li_zhuo@163.com>
2017-11-20 17:27:03 +0800

    修复demo.java语法错误

--- src/main/java/com/demo/demo.java
+++ src/main/java/com/demo/demo.java
@@ -1,6 +1,6 @@
-package com.demo
+package com.demo;
 
-public class Demo() {
+public class Demo {
 
     public static void main(int argc, String[] args) {
         System.out.println("Hello, world!");
